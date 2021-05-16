7160f466b4ad25f79ef7535693b711b00a707d7a
Balalaykajazz <archer2000@mail.ru>
2021-05-16 14:26:21 +0300

    getting message from mqtt refactoring

--- pod_mqtt_publisher/message_listener.py
+++ pod_mqtt_publisher/message_listener.py
@@ -1,17 +1,20 @@
 """This module is used to listen on a port to receive a message to write to the broker."""
-from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR, gaierror
+import socket
 import ssl
 import json
 import time
 from user_auth import client_authenticate
 from event_logger import get_logger  # type: ignore
-from mqtt_writer import publish_to_mqtt  # type: ignore
+from mqtt_writer import publish_to_mqtt, read_from_mqtt  # type: ignore
 from config import get_settings  # type: ignore
 
-MESSAGE_STATUS_SUCCESSFUL = "HTTP/1.1 200 OK"
-MESSAGE_STATUS_UNSUCCESSFUL = "HTTP/1.1 404 Not Found"
+MESSAGE_STATUS_SUCCESSFUL = "OK"
 INCORRECT_FORMAT_TITLE = "Incorrect format of the received file: %s"
 SLEEP_DURATION_AFTER_SENDING = 3
+AUTHENTICATION_CHECK = "/check_auth"
+CLIENT_WAITING_ANSWER = "/in/params"
+COUNT_OF_CHAR = len(CLIENT_WAITING_ANSWER)
+TOPIC_WITH_ANSWERS = "/out/info"
 
 event_log = get_logger("__listener__")
 
@@ -27,7 +30,7 @@ class SocketConnection:
         self.host = settings.get("host")
         self.port = settings.get("port")
 
-        self.server_socket = socket(AF_INET, SOCK_STREAM)
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         if settings.get("use_ssl"):
             self.server_socket = ssl.wrap_socket(self.server_socket,
@@ -36,13 +39,13 @@ class SocketConnection:
                                                  server_side=True)
 
     def __enter__(self):
-        self.server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
         try:
             self.server_socket.bind((self.host, self.port))
         except PermissionError as err:
             raise SocketConnectionError from err
-        except gaierror as err:
+        except socket.gaierror as err:
             raise SocketConnectionError from err
 
         self.server_socket.listen(1)
@@ -58,8 +61,73 @@ def is_correct_format_message(received_message: dict) -> bool:
     return sorted(list(received_message.keys())) == ["message", "password", "topic", "user"]
 
 
+def message_handling(request: str, settings_to_publish: dict) -> str:
+    """
+    Checking incoming message and publish to mqtt.
+    If topic contains CLIENT_WAITING_ANSWER we subscribe to TOPIC_WITH_ANSWERS
+
+    Operation's result is returned to client.
+    """
+
+    try:
+        received_message = json.loads(request)
+    except json.decoder.JSONDecodeError as err:
+        event_log.error(INCORRECT_FORMAT_TITLE, str(err))
+        return "Message wrong format"
+
+    if not is_correct_format_message(received_message):
+        answer_for_client = "The message does not contain a required field"
+        event_log.error(INCORRECT_FORMAT_TITLE, answer_for_client)
+        return answer_for_client
+
+    if not client_authenticate(received_message.get("user"),
+                               received_message.get("password")):
+        answer_for_client = "Unknown username or password"
+        event_log.error(answer_for_client)
+        return answer_for_client
+
+    report = received_message.get("topic"), received_message.get("message")
+    if report[1] == AUTHENTICATION_CHECK:
+        event_log.info("login user: %s", received_message.get("user"))
+        return MESSAGE_STATUS_SUCCESSFUL
+
+    successful = publish_to_mqtt(report, settings_to_publish)
+
+    if successful and report[0][-COUNT_OF_CHAR:] == CLIENT_WAITING_ANSWER:
+        # get answer from device
+        topic_with_answer = report[0][:-COUNT_OF_CHAR] + TOPIC_WITH_ANSWERS
+        return read_from_mqtt(settings=settings_to_publish,
+                              topic=topic_with_answer)
+
+    return MESSAGE_STATUS_SUCCESSFUL if successful else "Can't publish message."
+
+
+def open_socket(settings_to_socket: dict, settings_to_publish: dict) -> None:
+    """
+    listen on a port to receive a message. Received message must be a dictionary
+    """
+
+    try:
+        with SocketConnection(settings_to_socket) as server_socket:
+
+            while True:
+                conn, _ = server_socket.accept()
+                request = conn.recv(1024).decode("utf-8")
+
+                response = message_handling(request, settings_to_publish)
+
+                conn.sendall(response.encode())
+                conn.close()
+                time.sleep(SLEEP_DURATION_AFTER_SENDING)
+
+    except SocketConnectionError as err:
+        event_log.error("Socket connection error. Can't receive message. Reason: %s", str(err))
+    except socket.timeout:
+        open_socket(settings_to_socket, settings_to_publish)
+
+
 def start_listening() -> None:
-    """listen on a port to receive a message. Received message must be a dictionary"""
+    """Get settings and open socket"""
 
     settings_to_socket = get_settings("settings_to_socket")
     settings_to_publish = get_settings("settings_to_publish")
@@ -68,40 +136,6 @@ def start_listening() -> None:
                    settings_to_socket.get("host"),
                    settings_to_socket.get("port"))
 
-    try:
-        with SocketConnection(settings_to_socket) as server_socket:
-            while True:
-
-                conn, _ = server_socket.accept()
-                request = conn.recv(1024).decode("utf-8")
-
-                try:
-                    received_message = json.loads(request)
-                except json.decoder.JSONDecodeError as err:
-                    event_log.error(INCORRECT_FORMAT_TITLE, str(err))
-
-                if not is_correct_format_message(received_message):
-                    err_str = "Does not contain a required field"
-                    event_log.error(INCORRECT_FORMAT_TITLE, err_str)
-                    successful = False
-                elif not client_authenticate(received_message.get("user"),
-                                             received_message.get("password")):
-                    err_str = "Unknown username or password"
-                    event_log.error(err_str)
-                    successful = False
-                else:
-                    report = received_message.get("topic"), received_message.get("message")
-                    if report[0] == "" and report[1] == "test_auth_message":
-                        successful = True
-                    else:
-                        successful = publish_to_mqtt(report, settings_to_publish)
-
-                response = MESSAGE_STATUS_SUCCESSFUL if successful else MESSAGE_STATUS_UNSUCCESSFUL
-                conn.sendall(response.encode())
-                conn.close()
-                time.sleep(SLEEP_DURATION_AFTER_SENDING)
-
-    except SocketConnectionError as err:
-        event_log.error("Socket connection error. Can't receive message. Reason: %s", str(err))
+    open_socket(settings_to_socket, settings_to_publish)
 
     event_log.info("End working")
--- pod_mqtt_publisher/mqtt_writer.py
+++ pod_mqtt_publisher/mqtt_writer.py
@@ -1,9 +1,14 @@
 """This module is used to post messages to mqtt broker"""
 from socket import gaierror
+import multiprocessing  # type: ignore
+from multiprocessing.queues import Queue as mQueue  # type: ignore
+from datetime import datetime
 import paho.mqtt.client as mqtt  # type: ignore
+import paho.mqtt.subscribe as subscribe  # type: ignore
 from event_logger import get_logger  # type: ignore
 
 event_log = get_logger("__mqtt_writer__")
+TIMEOUT_WAIT_MQTT = 15
 
 
 class MQTTConnectionError(Exception):
@@ -59,3 +64,54 @@ def publish_to_mqtt(report: tuple, settings: dict) -> bool:
         return False
 
     return info.is_published()
+
+
+def read_from_mqtt(settings: dict, topic: str) -> str:
+    """
+    Получим ответ из mqtt с помощью функции get_answer_from_mqtt.
+    Функция является блокирующей, поэтому предусмотрим возможность отказаться от ожидания ответа.
+    Для этого создаем процесс, и контролируем время его работы. Если оно превышает TIME_LIMIT, то
+    останавливаем процесс.
+
+    Возвращаемое значение: строка с полученным ответом или сообщением о таймауте.
+    """
+
+    ctx = multiprocessing.get_context('spawn')
+    result_queue = ctx.Queue()
+    proc = ctx.Process(target=get_answer_from_mqtt,
+                       args=(result_queue,
+                             topic,
+                             settings.get("broker_settings"),
+                             settings.get("tls")))
+    proc.start()
+
+    start = datetime.now()
+    while (datetime.now() - start).seconds < TIMEOUT_WAIT_MQTT:
+        if not proc.is_alive():
+            answer = result_queue.get()
+            break
+
+    else:
+        answer = "Таймаут получения ответа от брокера"
+        proc.terminate()
+
+    return answer
+
+
+def get_answer_from_mqtt(reply_queue: mQueue,
+                         topic: str,
+                         broker_settings: dict,
+                         tls: dict) -> None:
+    """
+    Функция subscribe.simple() получает ответ из mqtt, который помещается в очередь.
+    """
+
+    answer = subscribe.simple(topic,
+                              hostname=broker_settings["host"],
+                              port=broker_settings["port"],
+                              retained=False,
+                              msg_count=1,
+                              tls=tls)
+
+    event_log.info("Received message from topic %s", topic)
+    reply_queue.put(answer.payload.decode())
