7270b80560a5da65af0e615c180d393d28973d67
Balalaykajazz <archer2000@mail.ru>
2021-05-18 00:03:17 +0300

    upd auth

--- pod_mqtt_publisher/user_auth.py
+++ pod_mqtt_publisher/user_auth.py
@@ -3,26 +3,20 @@ Client authorization on the server
 """
 
 import hmac
-import base64
 from config import get_settings
 
-CODE = "latin_1"
 
-
-def client_authenticate(client_user: str, client_token_b64: str) -> bool:
+def client_authenticate(client_user: str, received_token_hash: str) -> bool:
     """
     Login and password verification.
     User must be specified in the system.
     The password received from the client must match the server password.
     """
 
-    server_token_b64 = users.get(client_user)
+    correct_token_hash = users.get(client_user)
 
-    if not server_token_b64:
+    if not correct_token_hash or not received_token_hash:
         return False
-
-    correct_token_hash = base64.b64decode(server_token_b64)
-    received_token_hash = base64.b64decode(client_token_b64)
 
     return hmac.compare_digest(correct_token_hash, received_token_hash)
 
